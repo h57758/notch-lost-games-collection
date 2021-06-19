241e35e1a3da6858b4b4ec6967282d674b7b9580
Super Games Studios <63967786+h57758@users.noreply.github.com>
2021-06-19 23:41:46 +0200

    Removed Graphic From BUILD_PoC.bat

--- BUILD_PoC.bat
+++ BUILD_PoC.bat
@@ -1,17 +1,5 @@
 @echo off
-echo.
-echo.
-echo /---------------------------\
-echo : Prelude Of The Chambered! :
-echo \___________________________/
-echo.
-echo  ___________________________
-echo / Press Any Key To Start... \
-echo \___________________________/
-echo.
-echo      [ (C) 2011 Mojang ]
-pause>nul
-title Prelude Of The Chambered! Console
+title Prelude of the Chambered! Console
 cls
 cd "PoC_SRC"
 ..\ant\bin\ant run
