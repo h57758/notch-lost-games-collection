bc58b251e6f43df5ef65bb5ff3f5f531ea944c3f
Super Games Studios <63967786+h57758@users.noreply.github.com>
2021-06-19 23:19:42 +0200

    Changed README.md And Added Games

---
+++ BUILD_PoC.bat
@@ -0,0 +1,17 @@
+@echo off
+echo.
+echo.
+echo /---------------------------\
+echo : Prelude Of The Chambered! :
+echo \___________________________/
+echo.
+echo  ___________________________
+echo / Press Any Key To Start... \
+echo \___________________________/
+echo.
+echo      [ (C) 2011 Mojang ]
+pause>nul
+title Prelude Of The Chambered! Console
+cls
+cd "PoC_SRC"
+..\ant\bin\ant run
